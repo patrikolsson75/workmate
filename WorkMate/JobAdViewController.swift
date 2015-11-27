6c112894f8e6ab11030b0e9b2488990e47e6857a
Patrik Olsson <patrik@in2objects.com>
2015-11-27 20:54:35 +0100

    Created TableSection in Ad Detail View

--- WorkMate/JobAdViewController.swift
+++ WorkMate/JobAdViewController.swift
@@ -9,10 +9,16 @@
 import UIKit
 import Alamofire
 
+struct TableSection {
+    var title : String?
+    var cells : Array<UITableViewCell> = []
+}
+
 class JobAdViewController: UITableViewController {
 
     var annonsid : String?
     var platsannons : Platsannons?
+    var tableSections : Array<TableSection> = []
     var mediumDateFormatter : NSDateFormatter {
         let formatter = NSDateFormatter()
         formatter.dateStyle = .ShortStyle
@@ -49,129 +55,21 @@ class JobAdViewController: UITableViewController {
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
 
-        return 6
+        return self.tableSections.count
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 
-        return 1
+        return self.tableSections[section].cells.count
     }
 
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        
-        if indexPath.section == 0 {
-            let cell = tableView.dequeueReusableCellWithIdentifier("adHeaderCell", forIndexPath: indexPath) as! AdHeaderTableViewCell
-            cell.titleLabel.text = self.platsannons?.annonsrubrik
-            cell.workNameLabel.text = self.platsannons?.arbetsplats?.arbetsplatsnamn
-            
-            
-            var descriptionTexts : Array<String> = []
-            if let publiceraddatum = self.platsannons?.publiceraddatum {
-                descriptionTexts.append("Publicerad \(self.mediumDateFormatter.stringFromDate(publiceraddatum))")
-            }
-            if let ort = self.platsannons?.kommunnamn {
-                descriptionTexts.append(ort)
-            }
-            if let antal_platser = self.platsannons?.antal_platser {
-                descriptionTexts.append("Antal platser \(antal_platser)")
-            }
-            cell.descriptionLabel.text = descriptionTexts.joinWithSeparator(" ● ")
-            
-            if let sista_ansoknings_datum = self.platsannons?.ansokan?.sista_ansokningsdag {
-                cell.applyLastLabel.text = "Ansök senast \(self.mediumDateFormatter.stringFromDate(sista_ansoknings_datum))"
-            } else {
-                cell.applyLastLabel.text = "Ingen sista ansökningsdag"
-            }
-            return cell
-        } else if indexPath.section == 1 {
-            let cell = tableView.dequeueReusableCellWithIdentifier("adTextICell", forIndexPath: indexPath) as! LabelTableViewCell
-            cell.textView.text = self.platsannons?.annonstext
-            return cell
-        } else if indexPath.section == 2 {
-            let cell = tableView.dequeueReusableCellWithIdentifier("adTextICell", forIndexPath: indexPath) as! LabelTableViewCell
-            
-            var workTimeTexts : Array<String> = []
-            if let varaktighet = self.platsannons?.villkor?.varaktighet {
-                workTimeTexts.append(varaktighet)
-            }
-            if let arbetstid = self.platsannons?.villkor?.arbetstid {
-                workTimeTexts.append(arbetstid)
-            }
-            cell.textView?.text = workTimeTexts.joinWithSeparator("\n")
-            return cell
-        } else if indexPath.section == 3 {
-            let cell = tableView.dequeueReusableCellWithIdentifier("adTextICell", forIndexPath: indexPath) as! LabelTableViewCell
-            var textRows : Array<String> = []
-            if let lonetyp = self.platsannons?.villkor?.lonetyp {
-                textRows.append(lonetyp)
-            }
-            if let loneform = self.platsannons?.villkor?.loneform {
-                textRows.append(loneform)
-            }
-            cell.textView?.text = textRows.joinWithSeparator("\n")
-            return cell
-        } else if indexPath.section == 4 {
-            let cell = tableView.dequeueReusableCellWithIdentifier("adTextICell", forIndexPath: indexPath) as! LabelTableViewCell
-            var textRows : Array<String> = []
-            if let referens = self.platsannons?.ansokan?.referens where referens != "" {
-                textRows.append("Ange referens: \(referens)")
-            }
-            if let epostadress = self.platsannons?.ansokan?.epostadress {
-                textRows.append(epostadress)
-            }
-            if let webbplats = self.platsannons?.ansokan?.webbplats {
-                textRows.append(webbplats)
-            }
-            if let ovrigt_om_ansokan = self.platsannons?.ansokan?.ovrigt_om_ansokan {
-                textRows.append(ovrigt_om_ansokan)
-            }
-            cell.textView?.text = textRows.joinWithSeparator("\n")
-            return cell
-        } else {
-            let cell = tableView.dequeueReusableCellWithIdentifier("adTextICell", forIndexPath: indexPath) as! LabelTableViewCell
-            var textRows : Array<String> = []
-            if let arbetsplatsnamn = self.platsannons?.arbetsplats?.arbetsplatsnamn {
-                textRows.append(arbetsplatsnamn)
-            }
-            if let postadress = self.platsannons?.arbetsplats?.postadress {
-                textRows.append(postadress)
-            }
-            if let postnummer = self.platsannons?.arbetsplats?.postnummer, let postort = self.platsannons?.arbetsplats?.postort {
-                textRows.append("\(postnummer) \(postort)")
-            }
-            if let postland = self.platsannons?.arbetsplats?.postland {
-                textRows.append(postland)
-            }
-            if let besoksadress = self.platsannons?.arbetsplats?.besoksadress {
-                textRows.append("Besöksadress: \(besoksadress)")
-            }
-            if let hemsida = self.platsannons?.arbetsplats?.hemsida {
-                textRows.append(hemsida)
-            }
-            if let epostadress = self.platsannons?.arbetsplats?.epostadress {
-                textRows.append(epostadress)
-            }
-            cell.textView?.text = textRows.joinWithSeparator("\n")
-            return cell
-        }
-        
+        return self.tableSections[indexPath.section].cells[indexPath.row]
     }
     
     override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        if section == 2 {
-            return "Varaktighet, arbetstid"
-        }
-        if section == 3 {
-            return "Lön"
-        }
-        if section == 4 {
-            return "Ansökan"
-        }
-        if section == 5 {
-            return "Arbetsplats"
-        }
-        return nil
+        return self.tableSections[section].title
     }
     
     /*
@@ -230,10 +128,140 @@ class JobAdViewController: UITableViewController {
                         if let platsannons = try? Platsannons(jsonData: responseData) {
                             self.platsannons = platsannons
                             self.title = self.platsannons?.annonsrubrik
+                            self.tableSections.removeAll()
+                            self.tableSections.append(self.headerTableSection())
+                            self.tableSections.append(self.textTableSection())
+                            self.tableSections.append(self.workTimeTableSection())
+                            self.tableSections.append(self.paymentTableSection())
+                            self.tableSections.append(self.applyTableSection())
+                            self.tableSections.append(self.workPlaceTableSection())
                             self.tableView.reloadData()
                         }
                     }
             }
         }
+    }
+    
+    func headerTableSection() -> TableSection {
+        var tableSection = TableSection()
+
+        let cell = tableView.dequeueReusableCellWithIdentifier("adHeaderCell") as! AdHeaderTableViewCell
+        cell.titleLabel.text = self.platsannons?.annonsrubrik
+        cell.workNameLabel.text = self.platsannons?.arbetsplats?.arbetsplatsnamn
+        
+        
+        var descriptionTexts : Array<String> = []
+        if let publiceraddatum = self.platsannons?.publiceraddatum {
+            descriptionTexts.append("Publicerad \(self.mediumDateFormatter.stringFromDate(publiceraddatum))")
+        }
+        if let ort = self.platsannons?.kommunnamn {
+            descriptionTexts.append(ort)
+        }
+        if let antal_platser = self.platsannons?.antal_platser {
+            descriptionTexts.append("Antal platser \(antal_platser)")
+        }
+        cell.descriptionLabel.text = descriptionTexts.joinWithSeparator(" ● ")
+        
+        if let sista_ansoknings_datum = self.platsannons?.ansokan?.sista_ansokningsdag {
+            cell.applyLastLabel.text = "Ansök senast \(self.mediumDateFormatter.stringFromDate(sista_ansoknings_datum))"
+        } else {
+            cell.applyLastLabel.text = "Ingen sista ansökningsdag"
+        }
+        tableSection.cells.append(cell)
+        return tableSection
+    }
+    
+    func textTableSection() -> TableSection {
+        var tableSection = TableSection()
+        let cell = tableView.dequeueReusableCellWithIdentifier("adTextICell") as! LabelTableViewCell
+        cell.textView.text = self.platsannons?.annonstext
+        tableSection.cells.append(cell)
+        return tableSection
+    }
+    
+    func workTimeTableSection() -> TableSection {
+        var tableSection = TableSection()
+        tableSection.title = "Varaktighet, arbetstid"
+        let cell = tableView.dequeueReusableCellWithIdentifier("adTextICell") as! LabelTableViewCell
+        
+        var workTimeTexts : Array<String> = []
+        if let varaktighet = self.platsannons?.villkor?.varaktighet {
+            workTimeTexts.append(varaktighet)
+        }
+        if let arbetstid = self.platsannons?.villkor?.arbetstid {
+            workTimeTexts.append(arbetstid)
+        }
+        cell.textView?.text = workTimeTexts.joinWithSeparator("\n")
+        tableSection.cells.append(cell)
+        return tableSection
+    }
+    
+    func paymentTableSection() -> TableSection {
+        var tableSection = TableSection()
+        tableSection.title = "Lön"
+        let cell = tableView.dequeueReusableCellWithIdentifier("adTextICell") as! LabelTableViewCell
+        var textRows : Array<String> = []
+        if let lonetyp = self.platsannons?.villkor?.lonetyp {
+            textRows.append(lonetyp)
+        }
+        if let loneform = self.platsannons?.villkor?.loneform {
+            textRows.append(loneform)
+        }
+        cell.textView?.text = textRows.joinWithSeparator("\n")
+        tableSection.cells.append(cell)
+        return tableSection
+    }
+    
+    func applyTableSection() -> TableSection {
+        var tableSection = TableSection()
+        tableSection.title = "Ansökan"
+        let cell = tableView.dequeueReusableCellWithIdentifier("adTextICell") as! LabelTableViewCell
+        var textRows : Array<String> = []
+        if let referens = self.platsannons?.ansokan?.referens where referens != "" {
+            textRows.append("Ange referens: \(referens)")
+        }
+        if let epostadress = self.platsannons?.ansokan?.epostadress {
+            textRows.append(epostadress)
+        }
+        if let webbplats = self.platsannons?.ansokan?.webbplats {
+            textRows.append(webbplats)
+        }
+        if let ovrigt_om_ansokan = self.platsannons?.ansokan?.ovrigt_om_ansokan {
+            textRows.append(ovrigt_om_ansokan)
+        }
+        cell.textView?.text = textRows.joinWithSeparator("\n")
+        tableSection.cells.append(cell)
+        return tableSection
+    }
+    
+    func workPlaceTableSection() -> TableSection {
+        var tableSection = TableSection()
+        tableSection.title = "Arbetsplats"
+        let cell = tableView.dequeueReusableCellWithIdentifier("adTextICell") as! LabelTableViewCell
+        var textRows : Array<String> = []
+        if let arbetsplatsnamn = self.platsannons?.arbetsplats?.arbetsplatsnamn {
+            textRows.append(arbetsplatsnamn)
+        }
+        if let postadress = self.platsannons?.arbetsplats?.postadress {
+            textRows.append(postadress)
+        }
+        if let postnummer = self.platsannons?.arbetsplats?.postnummer, let postort = self.platsannons?.arbetsplats?.postort {
+            textRows.append("\(postnummer) \(postort)")
+        }
+        if let postland = self.platsannons?.arbetsplats?.postland {
+            textRows.append(postland)
+        }
+        if let besoksadress = self.platsannons?.arbetsplats?.besoksadress {
+            textRows.append("Besöksadress: \(besoksadress)")
+        }
+        if let hemsida = self.platsannons?.arbetsplats?.hemsida {
+            textRows.append(hemsida)
+        }
+        if let epostadress = self.platsannons?.arbetsplats?.epostadress {
+            textRows.append(epostadress)
+        }
+        cell.textView?.text = textRows.joinWithSeparator("\n")
+        tableSection.cells.append(cell)
+        return tableSection
     }
 }
